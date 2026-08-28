88237a5cd8510a58247aaf4becd8bf0071c0515e
Neeraj M <neerajmuraly@gmail.com>
2026-08-28 11:47:40 +0530

    Modern c for beginners

---
+++ basics/hello world.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+
+for(int x=0;x<10;x++)
+{
+ if(x==90)
+{
+    printf("The ASCII value of c is %d\n",x);
+}
+else if(x!=90)
+{
+    printf("sisyphus\n");
+}
+else
+{
+    printf("bye\n");
+
+}
+return 0;
+}
+ }
---
+++ basics/hello.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    char mychar='c',mychar1='\n',mychar2='\t';
+    printf("%zu\n , %zu\n,%zu\n",mychar,mychar1,mychar2);
+   
+    return 0;
+}
